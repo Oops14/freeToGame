1f71bdb99e91539babfa37520a9d70e189bcc9de
Oops14 <ups14q@gmail.com>
2024-06-23 10:13:37 +0300

    refactored formik form

--- src/features/auth/ui/Login.tsx
+++ src/features/auth/ui/Login.tsx
@@ -1,79 +1,85 @@
-import { Formik } from 'formik'
+import { useFormik } from 'formik'
 import { Footer } from '../../../common/components/footer/Footer'
 import { Header } from '../../../common/components/header/Header'
 import style from './login.module.scss'
 
 export const Login = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validate: (values) => {
+            const errors = {}
+
+            if (!values.email) {
+                errors.email = 'Required'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                errors.email = 'Invalid email address'
+            }
+
+            if (!values.password) {
+                errors.password = 'Required'
+            } else if (values.password.length <= 6) {
+                errors.password = 'The minimum length of the password is 6'
+            }
+
+            return errors
+        },
+        onSubmit: (values, formikHelpers) => {
+            console.log(`Email: ${values.email}`)
+            console.log(`Password: ${values.password}`)
+
+
+            formikHelpers.setSubmitting(false)
+            formik.resetForm()
+        },
+    })
+
     return (
         <>
             <Header />
             <div className={style.login_page}>
                 <div className={'container ' + style.login_container}>
                     <div className={style.login_form}>
-                        <h4>Sing In</h4>
+                        <h4>Sign In</h4>
 
-                        <Formik
-                            initialValues={{ email: '', password: '' }}
-                            validate={(values) => {
-                                const errors = {
-                                    email: '',
-                                    password: '',
-                                }
-
-                                if (!values.email) {
-                                    errors.email = 'Required'
-                                } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-                                    errors.email = 'Invalid email address'
-                                }
-
-                                if (!values.password) {
-                                    errors.password = 'Required'
-                                } else if (values.password.length <= 6) {
-                                    errors.password = 'The minimum lenght of the password is 6'
-                                }
-
-                                return errors
-                            }}
-                            onSubmit={(values) => {
-                                console.log(values)
-                            }}>
-                            {({ values, handleSubmit, errors, touched, handleChange, handleBlur, isSubmitting }) => (
-                                <form onSubmit={handleSubmit}>
-                                    <div>
-                                        <input
-                                            className="input_text"
-                                            type="email"
-                                            name="email"
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            value={values.email}
-                                        />
-                                        {errors.email && touched.email && <div className="error">{errors.email}</div>}
-                                    </div>
-                                    <div>
-                                        <input
-                                            className="input_password"
-                                            type="password"
-                                            name="password"
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            value={values.password}
-                                        />
-                                        {errors.password && touched.password && (
-                                            <div className="error">{errors.password}</div>
-                                        )}
-                                    </div>
-                                    <div className={style.buttons_block}>
-                                        <button type="submit" className="btn" disabled={isSubmitting}>
-                                            Sign In
-                                        </button>
-                                        <button type="button" className="btn">
-                                            Sign Up
-                                        </button>
-                                    </div>
-                                </form>
-                            )}
-                        </Formik>
+                        <form onSubmit={formik.handleSubmit}>
+                            <div>
+                                <input
+                                    className="input_text"
+                                    type="email"
+                                    name="email"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.email}
+                                />
+                                {formik.errors.email && formik.touched.email && (
+                                    <div className="error">{formik.errors.email}</div>
+                                )}
+                            </div>
+                            <div>
+                                <input
+                                    className="input_password"
+                                    type="password"
+                                    name="password"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.password}
+                                />
+                                {formik.errors.password && formik.touched.password && (
+                                    <div className="error">{formik.errors.password}</div>
+                                )}
+                            </div>
+                            <div className={style.buttons_block}>
+                                <button type="submit" className="btn" disabled={formik.isSubmitting}>
+                                    Sign In
+                                </button>
+                                <button type="button" className="btn">
+                                    Sign Up
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
