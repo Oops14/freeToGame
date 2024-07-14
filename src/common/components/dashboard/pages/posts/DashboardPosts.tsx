ca24c42f6d86538b0361dff226333e52c51d8bd2
Oops14 <ups14q@gmail.com>
2024-07-14 13:27:00 +0300

    fixed style issue of article item

--- src/common/components/dashboard/pages/posts/DashboardPosts.tsx
+++ src/common/components/dashboard/pages/posts/DashboardPosts.tsx
@@ -6,7 +6,7 @@ import FullScreenDialog from '../../../popup/FullScreenDialog.tsx'
 import style from '../../dashboard.module.scss'
 const DashboardPosts = () => {
     // @ts-ignore
-    const posts = useSelector<AppRootStateType, PostType[]>((state) => state.posts.posts);
+    const posts = useSelector<AppRootStateType, PostType[]>((state) => state.posts.posts)
 
     return (
         <>
@@ -15,7 +15,7 @@ const DashboardPosts = () => {
             </div>
             <div className={style.filter_panel}>
                 <div className={style.add_new}>
-                    <FullScreenDialog text={'Add new Post'}/>
+                    <FullScreenDialog text={'Add new Post'} />
                     <button className="btn">Remove Post</button>
                 </div>
                 <div className="filter">
@@ -30,18 +30,14 @@ const DashboardPosts = () => {
             {/* Panel */}
             <div className={style.post_item}>
                 <div className={style.post_block_title}>
-                    <input type="checkbox"/>
-                    <div className="post_title">
-                        Post Title
-                    </div>
+                    <input type="checkbox" />
+                    <div className="post_title">Post Title</div>
                 </div>
                 <div className={style.author}>
                     <span>Posted by</span>
                 </div>
                 <div className={style.categories}>Category</div>
-                <div className={style.comments}>
-                    Comments
-                </div>
+                <div className={style.comments}>Comments</div>
                 <div className={style.date}>
                     <span>Date</span>
                     <span> </span>
@@ -50,7 +46,7 @@ const DashboardPosts = () => {
 
             <div className={style.current_content_area}>
                 {posts.map((post) => {
-                    return <Post key={post.id} title={post.title} category={post.category} date={post.date}/>
+                    return <Post key={post.id} title={post.title} category={post.category} date={post.date} />
                 })}
             </div>
         </>
